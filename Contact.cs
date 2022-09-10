9a6f44c778ef3b6930e43706a435272f5d119935
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-10 06:53:37 +0530

    Contact.cs

---
+++ Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string city { get; set; }
+        public string Zip { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+
+
+
+    }
+}
+
+
+
